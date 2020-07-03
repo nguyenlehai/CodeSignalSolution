47bb73653726b296169624645c2ca80607304898
HaiNguyen <nguyenlehai@hotmail.com>
2020-07-03 15:12:41 +0700

    Arcade code java.

--- src/level_009_DarkWilderness/Main.java
+++ src/level_009_DarkWilderness/Main.java
@@ -4,5 +4,8 @@ public class Main {
   public static void main(String[] args) {
 	E_038_growingPlant e_038_growingPlant = new E_038_growingPlant();
 	e_038_growingPlant.growingPlant(100, 10, 4);
+
+	E_039_knapsackLight e_039_knapsackLight = new E_039_knapsackLight();
+	e_039_knapsackLight.knapsackLight(10, 5, 6, 4, 8);
   }
 }
