468c87452faa54e462dc4d836c65c576ca8f40fa
HaiNguyen <nguyenlehai@hotmail.com>
2020-07-06 11:19:21 +0700

    Add code of E_043_isBeautifulString.

---
+++ src/level_010_EruptionOfLight/Main.java
@@ -0,0 +1,8 @@
+package level_010_EruptionOfLight;
+
+public class Main {
+  public static void main(String[] args) {
+	E_043_isBeautifulString e_043_isBeautifulString = new E_043_isBeautifulString();
+	e_043_isBeautifulString.isBeautifulString("bbbaacdafe");
+  }
+}
