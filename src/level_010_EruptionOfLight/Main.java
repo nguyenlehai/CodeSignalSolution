5fee394a24a25a1aa533d0ed40c5d12314bec7e4
HaiNguyen <nguyenlehai@hotmail.com>
2020-07-06 11:37:10 +0700

    Add code of E_044_findEmailDomain.

--- src/level_010_EruptionOfLight/Main.java
+++ src/level_010_EruptionOfLight/Main.java
@@ -4,5 +4,8 @@ public class Main {
   public static void main(String[] args) {
 	E_043_isBeautifulString e_043_isBeautifulString = new E_043_isBeautifulString();
 	e_043_isBeautifulString.isBeautifulString("bbbaacdafe");
+
+	E_044_findEmailDomain e_044_findEmailDomain = new E_044_findEmailDomain();
+	e_044_findEmailDomain.findEmailDomain("\"very.unusual.@.unusual.com\"@usual.com");
   }
 }
