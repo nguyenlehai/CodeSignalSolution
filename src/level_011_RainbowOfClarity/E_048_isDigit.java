b4eb22203a03066608d1d6799ee46eb04fe08c95
HaiNguyen <nguyenlehai@hotmail.com>
2020-07-06 17:27:07 +0700

    Add code of E_048_isDigit.

---
+++ src/level_011_RainbowOfClarity/E_048_isDigit.java
@@ -0,0 +1,17 @@
+package level_011_RainbowOfClarity;
+
+public class E_048_isDigit {
+  /*
+Determine if the given character is a digit or not.
+----------------
+Example:
+For symbol = '0', the output should be
+isDigit(symbol) = true;
+
+For symbol = '-', the output should be
+isDigit(symbol) = false.
+   */
+  public boolean isDigit(char symbol) {
+	return Character.isDigit(symbol);
+  }
+}
---
+++ src/level_011_RainbowOfClarity/Main.java
@@ -0,0 +1,8 @@
+package level_011_RainbowOfClarity;
+
+public class Main {
+  public static void main(String[] args) {
+	E_048_isDigit e_048_isDigit = new E_048_isDigit();
+	e_048_isDigit.isDigit('0');
+  }
+}
